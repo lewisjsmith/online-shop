4f33434fe59c64bd5ee8f5578de3e0717a5c31d5
lewis <sendto.lewissmith@gmail.com>
2023-07-25 12:42:04 +0200

    Cart item number added to navbar. Array length used.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { CartContext } from "../App";
 import styles from "../styles/Navbar.module.css";
 
 function Navbar() {
+
+    const { cartItems } = useContext(CartContext);
 
     // Nav element?
     return (
@@ -11,7 +15,7 @@ function Navbar() {
                     <li><Link to={"/"}>Logo</Link></li>
                     <li><Link to={"/men"}>Men</Link></li>
                     <li><Link to={"/women"}>Women</Link></li>
-                    <li><Link to={"/cart"}>Cart</Link></li>
+                    <li><Link to={"/cart"}>Cart {cartItems.length === 0 ? "" : cartItems.length}</Link></li>
                 </ul>
             </nav>
             <hr></hr>
