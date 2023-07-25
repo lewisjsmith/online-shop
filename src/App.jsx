a3fd2da271f421b324b29bff3d2756179cf7b69d
lewis <sendto.lewissmith@gmail.com>
2023-07-25 12:37:37 +0200

    Context wrapped added. Skeleton array and function added for cart updates.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import ErrorPage from './components/ErrorPage'
@@ -6,10 +6,20 @@ import Cart from './components/Cart'
 import StorePage from './components/StorePage'
 import Home from './components/Home'
 
+export const CartContext = createContext({
+  cartItems: [],
+  addToCart: () => { }
+});
+
 function App() {
 
+  const [cartItems, setCartItems] = useState([]);
+  const addToCart = () => {
+    return;
+  }
+
   return (
-    <>
+    <CartContext.Provider value={{ cartItems, addToCart }}>
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
@@ -23,7 +33,7 @@ function App() {
         </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </>
+    </CartContext.Provider>
   )
 }
 
