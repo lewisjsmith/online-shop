96e1e317db189a6b4fa7e38ca3efeb4acc778d5c
lewis <sendto.lewissmith@gmail.com>
2023-08-01 22:29:35 +0200

    Sorts by price and alphabetical order.

--- src/components/StorePage.jsx
+++ src/components/StorePage.jsx
@@ -10,6 +10,7 @@ export default function StorePage() {
 
     const { products } = useContext(ShopContext);
 
+    const [productSort, setProductSort] = useState(null);
     const [productList, setProductList] = useState([]);
 
     useEffect(() => {
@@ -19,33 +20,86 @@ export default function StorePage() {
     const obj = useLocation();
     const categoriesList = createFilter(obj.pathname);
 
+    function handleChange(e) {
+        setProductSort(e.target.value);
+    }
+
+    const sortedProducts = productSort ? productList.sort((a, b) => {
+        switch (productSort) {
+            case ("highest"):
+                if (a.price < b.price) {
+                    return 1
+                } else if (a.price > b.price) {
+                    return -1
+                } else {
+                    return 0;
+                }
+                break;
+            case ("lowest"):
+                if (a.price < b.price) {
+                    return -1
+                } else if (a.price > b.price) {
+                    return 1
+                } else {
+                    return 0;
+                }
+                break;
+            case ("az"):
+                if (a.value.charAt(0) < b.value.charAt(0)) {
+                    return -1
+                } else if (a.value.charAt(0) > b.value.charAt(0)) {
+                    return 1
+                } else {
+                    return 0;
+                }
+                break;
+            case ("za"):
+                if (a.value.charAt(0) < b.value.charAt(0)) {
+                    return 1
+                } else if (a.value.charAt(0) > b.value.charAt(0)) {
+                    return -1
+                } else {
+                    return 0;
+                }
+        }
+    }) : productList;
+
     return (
-        <ul className={styles["store-list"]}>
-            {categoriesList.length === 2 ? (
-                productList.filter(item => item.categories.includes(categoriesList[0]))
-                    .filter(item => item.categories.includes(categoriesList[1])).map(item => {
-                        {
-                            return (
-                                <li className={styles["list-item"]} key={item.value}>
-                                    <Card item={item} src={item.srcOne} price={item.price}/>
-                                </li>
-                            );
+        <div>
+            <select onChange={handleChange}>
+                <option value={null}>By Release Date</option>
+                <option value={"highest"}>Price highest to lowest</option>
+                <option value={"lowest"}>Price lowest to highest</option>
+                <option value={"az"}>Name A to Z</option>
+                <option value={"za"}>Name Z to A</option>
+            </select>
+            <ul className={styles["store-list"]}>
+                {sortedProducts.length === 2 ? (
+                    productList.filter(item => item.categories.includes(categoriesList[0]))
+                        .filter(item => item.categories.includes(categoriesList[1])).map(item => {
+                            {
+                                return (
+                                    <li className={styles["list-item"]} key={item.value}>
+                                        <Card item={item} src={item.srcOne} price={item.price} />
+                                    </li>
+                                );
+                            }
+                        })
+                ) : (
+                    sortedProducts
+                        .filter(item => item.categories.includes(categoriesList[0])).map(item => {
+                            {
+                                return (
+                                    <li className={styles["list-item"]} key={item.value}>
+                                        <Card item={item} src={item.srcOne} price={item.price} />
+                                    </li>
+                                );
+                            }
                         }
-                    })
-            ) : (
-                productList
-                    .filter(item => item.categories.includes(categoriesList[0])).map(item => {
-                        {
-                            return (
-                                <li className={styles["list-item"]} key={item.value}>
-                                    <Card item={item} src={item.srcOne} price={item.price}/>
-                                </li>
-                            );
-                        }
-                    }
-                    )
-            )
-            }
-        </ul>
+                        )
+                )
+                }
+            </ul>
+        </div>
     );
 }
