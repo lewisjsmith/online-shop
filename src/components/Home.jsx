3be2bc9c27893e1883e88f54d5cac8109d76975f
lewis <sendto.lewissmith@gmail.com>
2023-08-03 19:20:32 +0200

    Multi-video looper replaced by one merged video clip due to white flashes on forced reload.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
-import bg from "../assets/test-video.mp4";
+import vid from "../assets/video-merged.mp4"
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
 
+    // const [index, setIndex] = useState(0);
+    // const [src, setSrc] = useState(vid1);
+    // const videoRef = useRef(null);
+
+    // const videoList = [vid1, vid2, vid3, vid4];
+
+    // function handleOnEnded() {
+    //     if(index === 3) {
+    //         setIndex(0);
+    //     } else {
+    //         setIndex(index + 1);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     setSrc(videoList[index]);
+    // }, [index])
+
+    // useEffect(() => {
+    //     videoRef.current.load();
+    // }, [src])
+
     return (
-        <div style={{
+        <div className={styles["home-page"]} style={{
             width: "100%",
             height: "100%",
         }}>
@@ -18,7 +40,7 @@ export default function Home() {
                 objectFit: "contain",
                 overflow: "hidden"
             }}>
-                <video autoPlay muted style={{
+                <video autoPlay loop muted style={{
                     minWidth: "100%",
                     minHeight: "100%",
                     width: "auto",
@@ -29,7 +51,7 @@ export default function Home() {
                     transform: "translate(-50%,-50%)",
                     zIndex: "-1"
                 }}>
-                    <source src={bg} type="video/mp4" />
+                    <source src={vid} type="video/mp4" />
                 </video>
             </div>
             <div className={styles["button-wrapper"]}>
