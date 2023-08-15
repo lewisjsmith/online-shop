6737872889e9100078299281e25e0ed03a2411cd
lewis <sendto.lewissmith@gmail.com>
2023-08-15 18:55:38 +0200

    Home page mobile sizing.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,34 +11,31 @@ export default function Home() {
     const { windowQuery } = useContext(ShopContext);
 
     return (
-        <div className={styles["home-page"]} style={{
-            width: "100%",
-            height: "100%",
-        }}>
-            {windowQuery.matches && <div style={{
-                width: "100vw",
-                height: "100vh",
-                objectFit: "contain",
-                overflow: "hidden"
-            }}>
-                <video autoPlay loop muted style={{
-                    minWidth: "100%",
-                    minHeight: "100%",
-                    width: "auto",
-                    height: "auto",
-                    position: " absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%,-50%)",
-                    zIndex: "-2"
-                }}>
-                    <source src={vid} type="video/mp4" />
-                </video>
-                <div className={styles["button-wrapper"]}>
-                    <Link to={"/men/"}><button className={styles["left"]} style={{ textDecoration: "none" }}>SHOP MEN</button></Link>
-                    <Link to={"/women/"}><button className={styles["right"]} style={{ textDecoration: "none" }}>SHOP WOMEN</button></Link>
+        <div className={styles["home-page"]} style={windowQuery.matches ? {marginTop: "100px"} : {marginTop: "70px"}}>
+
+                <div style={{height: "100%", width: "100%"}}>
+
+                    <div className={styles["video-wrapper"]}>
+                        <video autoPlay loop muted className={styles["video"]}>
+                            <source src={vid} type="video/mp4" />
+                        </video>
+
+                        <div className={styles["center-text"]}>
+                            <h2>
+                                Effortless style.
+                            </h2>
+                            <h2>
+                                Responsibly sourced.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className={windowQuery.matches ? styles["button-wrapper"] : styles["button-wrapper-mobile"]}>
+                        <button className={windowQuery.matches ? styles["left"] : styles["left-mobile"]} style={{ textDecoration: "none" }}><Link to={"/men/"} style={{width: "100%"}}>SHOP MEN</Link></button>
+                        <button className={windowQuery.matches ? styles["right"] : styles["right-mobile"]} style={{ textDecoration: "none" }}><Link to={"/women/"} style={{width: "100%"}}>SHOP WOMEN</Link></button>
+                    </div>
+
                 </div>
-            </div>}
 
         </div>
 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/online-shop/',
+  base: '',
   plugins: [react()],
   test: {
     globals: true,
